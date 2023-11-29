847b0d6449ecff18a777c1de90ca4a62388fb263
MaskedGod <134432872+MaskedGod@users.noreply.github.com>
2023-11-30 01:52:06 +0400

    establishing lists for genertor

---
+++ D5.PassGen.py
@@ -0,0 +1,11 @@
+import random
+import string
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+symbols = list(string.punctuation)
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
