7ed4891ce2ee5b2651214bf7159240681a473f01
MaskedGod <134432872+MaskedGod@users.noreply.github.com>
2023-11-30 02:22:49 +0400

    all functionality implemented, done. input never checked

--- D5.PassGen.py
+++ D5.PassGen.py
@@ -9,3 +9,26 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+pass_list = []
+password = ""
+i = 0
+
+while i < nr_letters:
+    pass_list.append(str(random.choice(letters)))
+    i += 1
+i = 0
+
+while i < nr_symbols:
+    pass_list.append(str(random.choice(symbols)))
+    i += 1
+i = 0
+
+while i < nr_numbers:
+    pass_list.append(str(random.choice(numbers)))
+    i += 1
+
+random.shuffle(pass_list)
+for char in range(len(pass_list)-1):
+    password += pass_list[char]
+
+print(f"Your new password: \n{password}")
