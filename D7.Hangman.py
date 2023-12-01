81336fdd9482ed6771c4be10ad09f0be94d86ca8
MaskedGod <134432872+MaskedGod@users.noreply.github.com>
2023-12-02 03:22:20 +0400

    done and done put a noose on em

--- D7.Hangman.py
+++ D7.Hangman.py
@@ -1,14 +1,36 @@
 import random as rnd
+from D7HangmanMisc import art, guessing_word
 
-guessing_word = [
-    "Apple", "Beach", "Brain", "Bread", "Brush", "Chair",
-    "Chest", "Chord", "Click", "Clock", "Cloud", "Dance",
-    "Diary", "Drink", "Earth", "Flute", "Fruit", "Ghost",
-    "Grape", "Green", "Happy", "Heart", "House", "Juice",
-    "Light", "Money", "Music", "Party", "Pizza", "Plant",
-    "Radio", "River", "Salad", "Sheep", "Shoes", "Smile",
-    "Snack", "Snake", "Spice", "Spoon", "Storm", "Table",
-    "Toast", "Tiger", "Train", "Water", "Whale", "Wheel",
-    "Woman", "World", "Write", "Youth"
-    ]
-word = rnd.choice(guessing_word)
+end_of_game = False
+word = rnd.choice(guessing_word).lower()
+display = ["_" for i in range(len(word))]
+lifes = 5
+print(display)
+
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    #stop that madness 
+    if guess == "stop":
+        end_of_game = True
+    #Check guessed letter
+    for position in range(len(word)):
+        letter = word[position]
+        if letter == guess:
+            display[position] = letter
+    # If you guessed wrong it's one life gone
+    if guess not in word:
+        print(f'{guess} not in word')
+        lifes -= 1 
+        print(art[lifes])
+
+    print(display)
+
+    # R ya winning son?
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+    # Guess not(
+    elif lifes == 0:
+        end_of_game = True
+        print("Ya hanged:(")
