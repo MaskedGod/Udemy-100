69fe7f349c2cc6f264848d93c962dff45436b02a
MaskedGod <134432872+MaskedGod@users.noreply.github.com>
2023-12-05 20:30:49 +0400

    It's Auctioning

--- test.py
+++ test.py
@@ -1,34 +1,20 @@
 import os
-
+# os.system('cls')
 print("Welcome to the secret auction program.")
-
-def clear():
+def clearshell():
     os.system('cls')
-
 def highest_bidder():
     auction = True
     bidders = {}
-    
-    while auction:
+    while auction == True:
         name = input("What's your name: ")
         bid = int(input("What's your bid: $"))
         bidders[name] = bid
-        clear()
-        
+        clearshell()
         if input("Are there any other bidders? 'yes' or 'no' \n") == "no":
-            # auction = False
-            break
-        clear()
-        
+            auction = False
+        clearshell()
     for name, bid in bidders.items():
-        max = bid
-        if bid > max:
-            max = bid 
-    clear()     
-    print(f"The winner is {name} with ${max}")
-
-    if input("Want another auction? 'yes' or 'no' \n") == "yes":
-        clear()
-        highest_bidder()
-    
+        print(name)        
+    print(bidders)
 highest_bidder()
