9e4361d668b05d598f7ab4b5262b3e8a69437548
MaskedGod <134432872+MaskedGod@users.noreply.github.com>
2023-12-27 19:33:28 +0400

    starting strong with arts and data

---
+++ D14.HigherLower.py
@@ -0,0 +1,11 @@
+import os
+import random
+from D14Art import logo, versus
+from D14GameData import GAME_DATA 
+
+def clear():
+    """Clears the screen"""
+    os.system('cls')
+
+print(versus)
+print(GAME_DATA[0]["searches"])
